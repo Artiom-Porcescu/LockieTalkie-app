830bbd142369d0605b7ebedc2b4e6ddb520a3848
Artiom Porcescu <porchiesku@bk.ru>
2023-06-03 11:34:06 +0300

    Added Log In and Forgot password functionality

--- LockieTalkie/ForgotPasswordViewController.swift
+++ LockieTalkie/ForgotPasswordViewController.swift
@@ -6,12 +6,43 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class ForgotPasswordViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    var styling = UIStyling()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        styling.customizeButton(for: submitButton, color: UIColor(red: 0.72, green: 0.85, blue: 0.99, alpha: 1.0).cgColor, cornerRadius: 25)
+        
+        styling.customizeTextFields(for: [emailTextField], cornerRadius: 25, borderWidth: 2.0, borderColor: UIColor(red: 0.72, green: 0.85, blue: 0.99, alpha: 1.0).cgColor)
+    }
+    
+    
+    
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+        
+        let auth = Auth.auth()
+
+        auth.sendPasswordReset(withEmail: emailTextField.text!) { (error) in
+        if let error = error {
+        //self.errorLabel.text = "Please enter a valid email adress"
+        let alert = UIAlertController(title: "Oops", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        return
+        }
+        let alert = UIAlertController(title: "Great", message: "A password reset email has been sent", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        }
+        
+        navigationController?.popViewController(animated: true)
     }
 }
--- LockieTalkie/LoginViewController.swift
+++ LockieTalkie/LoginViewController.swift
@@ -6,13 +6,15 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseCore
 
 class LoginViewController: UIViewController {
     
     var styling = UIStyling()
-    
     var loginUserDefaults = UserDefaults.standard
     
+    @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
@@ -23,6 +25,21 @@ class LoginViewController: UIViewController {
         styling.customizeButton(for: loginButton, borderWidth: 2.0, color: UIColor(red: 0.72, green: 0.85, blue: 0.99, alpha: 1.0).cgColor, cornerRadius: 25)
         
         styling.customizeTextFields(for: [emailTextField, passwordTextField], cornerRadius: 25, borderWidth: 2.0, borderColor: UIColor(red: 0.72, green: 0.85, blue: 0.99, alpha: 1.0).cgColor)
+    }
+    
+    @IBAction func logInPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    let alert = UIAlertController(title: "Oops", message: e.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    return
+                } else {
+                    self.performSegue(withIdentifier: "goToChatLogged", sender: self)
+                }
+            }
+        }
     }
     
 }
